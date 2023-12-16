1bac2e737e61516d856d8bc9aa56c9dc92d3377c
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-16 13:27:34 +0500

    Create 300 cakes.py
    рабочий скрипт на выполнение ачивки "сделать 300 тортов"

---
+++ 300 cakes.py
@@ -0,0 +1,15 @@
+import pyautogui
+import keyboard
+
+stopping = False
+
+def pressed():
+    global stopping
+    stopping = True
+
+keyboard.add_hotkey("delete", lambda: pressed())
+
+while not stopping:
+    pyautogui.click(630, 930,duration=0)
+    pyautogui.click(1200, 840,duration=0)
+
