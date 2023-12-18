25f8244986616ec9a5045186ca6173db80075fcf
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-18 13:07:31 +0500

    оно уже может решить весь клевер, поэтому это надо залить

--- Memory match.py
+++ Memory match.py
@@ -7,8 +7,11 @@ try:
     files = os.listdir(r'png\stash')
     for item in range(len(files)):
         os.remove(fr'png\stash\{files[item]}')
+    files = os.listdir(r'png\stash\last')
+    for item in range(len(files)):
+        os.remove(fr'png\stash\{files[item]}')
     # os.makedirs(r'png\stash')
-    os.makedirs(r'png\stash\povar')
+    os.makedirs(r'png\stash\last')
 except FileNotFoundError:
     print()
 except Exception as e:
@@ -20,15 +23,44 @@ def matrix_coord(stolbik, ryad):
     b = matrix[8 * (ryad - 1) + (stolbik - 1)].get('y')
     return a, b
 
-def screen_All():
-    for i in matrix:
-        pyautogui.click(i['x'],i['y'])
+def anti_repeat_filter(list_of_unknowns):
+    ramx = list_of_unknowns[0][0]-114
+    ramy = list_of_unknowns[0][1]-114
+    count = 0
+    delete_list = []
+    for every in range(len(list_of_unknowns)):
+        count += 1
+        if list_of_unknowns[every][0] - ramx > 100 or list_of_unknowns[every][1] - ramy > 100:
+            ramx = list_of_unknowns[every][0]
+            ramy = list_of_unknowns[every][1]
+        else:
+            delete_list.append(every)
+
+    delete_list.reverse()
+    for index in delete_list:
+        list_of_unknowns.pop(index)
+    return list_of_unknowns
+
+
+def find_unknowns():
+    try:
+        clevers = list(pyautogui.locateAllOnScreen(r'png\important\clever.png', confidence=0.86))
+    except:
+        pass
+    clevers=anti_repeat_filter(clevers)
+    return clevers
+
+
+
+def screen_All(unknowns):
+    for every in unknowns:
+        pyautogui.click(every)
         time.sleep(0.4)
         position = pyautogui.position()
         name = f'{position[0]}, {position[1]}'
 
-        scr = pyautogui.screenshot(fr'png\stash\{name}.png', region=(int(position[0])-25, int(position[1])-25,
-                                                                     51, 51))
+        scr = pyautogui.screenshot(fr'png\stash\{name}.png', region=(int(position[0])-53, int(position[1])-53,
+                                                                     107, 107))
         try:
             pyautogui.locateOnScreen(rf'png\important\avoid.png', confidence=0.8)
             os.remove(fr'png\stash\{name}.png')
@@ -36,11 +68,16 @@ def screen_All():
             pass
         try:
             pyautogui.locateOnScreen(rf'png\important\avoid2.png', confidence=0.8)
-            os.replace(fr'png\stash\{name}.png',fr'png\stash\povar\{name}.png')
+            os.replace(fr'png\stash\{name}.png',fr'png\stash\last\{name}.png')
         except:
             pass
+        try:
+            pyautogui.locateOnScreen(rf'png\important\joker.png', confidence=0.8)
+            os.replace(fr'png\stash\{name}.png',fr'png\stash\last\{name}.png')
+        except:
+            pass
+        pyautogui.click(every)
 
-        pyautogui.click(i.get('x'), i.get('y'))
 
 
 def find_match():
@@ -51,50 +88,37 @@ def find_match():
             if item2 == item:
                 continue
             try:
-                pyautogui.locate(rf'png\stash\{str(item)}',rf'png\stash\{str(item2)}',confidence=0.7)
+                pyautogui.locate(rf'png\stash\{str(item)}',rf'png\stash\{str(item2)}',confidence=0.8)
                 coords = str(item)[:-4].split(', ')
-                pyautogui.click(int(coords[0]), int(coords[1]),duration=0.5)
+                pyautogui.click(int(coords[0]), int(coords[1]),duration=0.33)
                 coords2 = str(item2)[:-4].split(', ')
-                pyautogui.click(int(coords2[0]), int(coords2[1]),duration=0.5)
+                pyautogui.click(int(coords2[0]), int(coords2[1]),duration=0.33)
                 os.remove(rf'png\stash\{str(item)}')
                 os.remove(rf'png\stash\{str(item2)}')
-                files.remove(str(item))
-                files.remove(str(item2))
             except:
                 continue
-    files = os.listdir(r'png\stash\povar')
+    files = os.listdir(r'png\stash\last')
     for item2 in files:
         for item in files:
             if item2 == item:
                 continue
             try:
-                pyautogui.locate(rf'png\stash\povar\{str(item)}', rf'png\stash\povar\{str(item2)}', confidence=0.8)
+                pyautogui.locate(rf'png\stash\last\{str(item)}', rf'png\stash\last\{str(item2)}', confidence=0.8)
                 coords = str(item)[:-4].split(', ')
-                pyautogui.click(int(coords[0]), int(coords[1]), duration=0.5)
+                pyautogui.click(int(coords[0]), int(coords[1]), duration=0.33)
                 coords2 = str(item2)[:-4].split(', ')
-                pyautogui.click(int(coords2[0]), int(coords2[1]), duration=0.5)
-                os.remove(rf'png\stash\povar\{str(item)}')
-                os.remove(rf'png\stash\povar\{str(item2)}')
+                pyautogui.click(int(coords2[0]), int(coords2[1]), duration=0.33)
+                os.remove(rf'png\stash\last\{str(item)}')
+                os.remove(rf'png\stash\last\{str(item2)}')
             except:
                 continue
     time.sleep(3)
-    try:
-        for mixer in pyautogui.locateAllOnScreen(r'png\important\clever.png', confidence=0.85):
-            pyautogui.click(mixer)
-        time.sleep(4)
-        for mixer in pyautogui.locateAllOnScreen(r'png\important\clever.png', confidence=0.85):
-            pyautogui.click(mixer)
-    except:
-        pass
-    try:
-        for mixer in pyautogui.locateAllOnScreen(r'png\important\heart.png', confidence=0.85):
-            pyautogui.click(mixer)
-        time.sleep(4)
-        for mixer in pyautogui.locateAllOnScreen(r'png\important\heart.png', confidence=0.85):
-            pyautogui.click(mixer)
-    except:
-        pass
-
+    unknowns = find_unknowns()
+    for mixer in unknowns:
+        pyautogui.click(mixer)
+    time.sleep(4)
+    for mixer in unknowns:
+        pyautogui.click(mixer)
 
 
     # for i in range(0,8):
@@ -102,13 +126,13 @@ def find_match():
     #         pyautogui.click(matrix_coord(i,ii),duration=0.3)
 
 
-matrix = []
-
-for i in range(8): #8
-    for ii in range(8):
-        matrix.append({'x': 558 + 114 * ii, 'y': 120 + 114 * i})
-
-screen_All()
+# matrix = []
+#
+# for i in range(8): #8
+#     for ii in range(8):
+#         matrix.append({'x': 558 + 114 * ii, 'y': 120 + 114 * i})
+unknowns = find_unknowns()
+screen_All(unknowns)
 find_match()
 # print(len(matrix))
 # print(matrix[0].get('x'))
