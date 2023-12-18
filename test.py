40155e7f0b4b62f64d908207d257ef3b83d9a861
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-18 13:42:50 +0500

    update

--- test.py
+++ test.py
@@ -4,18 +4,15 @@ from PIL import Image, ImageDraw
 import os
 
 try:
-    clevers = list(pyautogui.locateAllOnScreen(r'png\important\clever.png', confidence=0.86))
+    clevers = list(pyautogui.locateAllOnScreen(r'png\important\smile.png', confidence=0.86))
 except:
     pass
+print(clevers[0][0])
+scr = pyautogui.screenshot(fr'png\stash\smile.png', region=(int(pyautogui.center(clevers[0][0]))-53, int(pyautogui.center(clevers[0][0]))-53,
+                                                                     107, 107))
 
-for i in clevers:
 
-    print(clevers.index(i), i)
 
-print()
-print()
-print()
-# print(clevers[0])
 # print(clevers[1])
 # print(type(clevers[1]))
 # print(clevers[0][1])
@@ -23,30 +20,30 @@ print()
 # print(int(clevers[0][0]))
 # print(clevers[46])
 
-ramx = 392
-ramy = -46
-count = 0
-delete_list = []
-for every in range(len(clevers)):
-    count += 1
-    # print(clevers)
-    if clevers[every][0] - ramx > 100 or clevers[every][1] - ramy > 100:
-        ramx = clevers[every][0]
-        ramy = clevers[every][1]
-    else:
-        delete_list.append(every)
-
-delete_list.reverse()
-
+# ramx = 392
+# ramy = -46
+# count = 0
+# delete_list = []
+# for every in range(len(clevers)):
+#     count += 1
+#     # print(clevers)
+#     if clevers[every][0] - ramx > 100 or clevers[every][1] - ramy > 100:
+#         ramx = clevers[every][0]
+#         ramy = clevers[every][1]
+#     else:
+#         delete_list.append(every)
+#
+# delete_list.reverse()
+#
+# # for i in clevers:
+# #
+# #     print(clevers.index(i), i)
+# for index in delete_list:
+#     clevers.pop(index)
+#
 # for i in clevers:
 #
 #     print(clevers.index(i), i)
-for index in delete_list:
-    clevers.pop(index)
-
-for i in clevers:
-
-    print(clevers.index(i), i)
 # def find_match():
 #     files = os.listdir(r'png\stash')
 #     for item in files:
